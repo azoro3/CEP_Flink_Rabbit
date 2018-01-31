2fd803536039d0d45f41d662996c073f5fb934ac
ARTHUR-PC\Arthur <arthur.rozo@outlook.fr>
2018-01-31 11:43:33 +0100

    perceptron

---
+++ src/main/java/FlinkWork.java
@@ -0,0 +1,39 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Arthur
+ */
+public class FlinkWork {
+
+    public void wordCount(String data) {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // get input data
+        DataSet<String> text = env.fromElements(data);
+
+        DataSet<Tuple2<String, Integer>> counts
+                = // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new ComaSpliter())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0)
+                        .sum(1);
+
+        try {
+            // execute and print result
+            counts.print();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
